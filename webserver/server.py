fc2e63a34edebba7afe4b682f2af432011aebbe4
Jace Yang <420478365@qq.com>
2022-03-28 22:58:03 -0400

    Update

--- webserver/server.py
+++ webserver/server.py
@@ -36,8 +36,8 @@ app = Flask(__name__, template_folder=tmpl_dir)
 # For your convenience, we already set it to the class database
 
 # Use the DB credentials you received by e-mail
-DB_USER = "YOUR_DB_USERNAME_HERE"
-DB_PASSWORD = "YOUR_DB_PASSWORD_HERE"
+DB_USER = "jy3174"
+DB_PASSWORD = "JaceYJH"
 
 DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
 
