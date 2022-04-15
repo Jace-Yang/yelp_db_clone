beb4a25fc50aaeaf365541e3e6f9505db5d896b1
Jace Yang <420478365@qq.com>
2022-04-15 15:38:21 -0400

    Change port

--- yealp_frontend/flaskblog/__init__.py
+++ yealp_frontend/flaskblog/__init__.py
@@ -19,6 +19,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
